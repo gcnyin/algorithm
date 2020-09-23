da8241049a3b36b935d32789e369ab26de196fa8
gcnyin <guchounongyin@gmail.com>
2020-09-23 23:18:26 +0800

    nextPermutation standrad solution

--- java/src/main/java/com/github/gcnyin/algo/nextpermutation/Solution.java
+++ java/src/main/java/com/github/gcnyin/algo/nextpermutation/Solution.java
@@ -1,37 +1,33 @@
 package com.github.gcnyin.algo.nextpermutation;
 
-import java.util.Arrays;
-import java.util.Objects;
-
 class Solution {
     public void nextPermutation(int[] nums) {
-        for (int i = nums.length - 1; i >= 0; i--) {
-            int cursor = nums[i];
-            for (int j = i - 1; j >= 0; j--) {
-                int nextCursor = nums[j];
-                if (cursor > nextCursor) {
-                    nums[i] = nextCursor;
-                    nums[j] = cursor;
-                    Arrays.sort(nums, j + 1, nums.length);
-                    return;
-                }
-            }
+        int i = nums.length - 2;
+        while (i >= 0 && nums[i + 1] <= nums[i]) {
+            i--;
         }
-        reserve(nums, 0, nums.length - 1);
+        if (i >= 0) {
+            int j = nums.length - 1;
+            while (j >= 0 && nums[j] <= nums[i]) {
+                j--;
+            }
+            swap(nums, i, j);
+        }
+        reverse(nums, i + 1);
     }
 
-    private void reserve(int[] nums, int i, int j) {
-        int k = 0;
-        while (true) {
-            int l = i + k;
-            int m = j - k;
-            if (Objects.equals(l, m) || l > m) {
-                break;
-            }
-            int tmp = nums[l];
-            nums[l] = nums[m];
-            nums[m] = tmp;
-            k++;
+    private void reverse(int[] nums, int start) {
+        int i = start, j = nums.length - 1;
+        while (i < j) {
+            swap(nums, i, j);
+            i++;
+            j--;
         }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
